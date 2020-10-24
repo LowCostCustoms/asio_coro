782ccdcdeec4923709b44187ffea1cacef1f6a9b
zhuebok <zhuebok@protonmail.com>
2020-10-24 16:48:52 +0700

    Don't look for the boost unit_test library

--- src/asio_coro/asio_coro.hpp
+++ src/asio_coro/asio_coro.hpp
@@ -3,14 +3,14 @@
 
 #include "async_accept.hpp"
 #include "async_connect.hpp"
+#include "async_mutex.hpp"
 #include "async_read.hpp"
 #include "async_wait.hpp"
-#include "async_write.hpp"
 #include "async_wait_signal.hpp"
+#include "async_write.hpp"
+#include "boost_future.hpp"
 #include "dispatch.hpp"
 #include "post.hpp"
 #include "task.hpp"
-#include "boost_future.hpp"
-#include "async_mutex.hpp"
 
 #endif // ASIO_CORO_EXTENSIONS_ASIO_CORO_HPP
